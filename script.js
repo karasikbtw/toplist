228c5e250596607e1ca8be49b8d5457bd5c92108
karasikbtw <drinjjy@gmail.com>
2025-06-29 15:19:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,34 +7,37 @@ let lists = [
             { "name": "?", "creator": "noone", "id": "?" },
             { "name": "?", "creator": "noone", "id": "?" }
         ],
-        { enum: 0 }
+        { name: "Список невозможных уровней - TopList", enum: 0 }
     ]
 ];
 
 lists.forEach(list => {
     let levels = list[0];
     let globEnum = list[1].enum;
+    let listname = list[1].name;
 
-    levels.forEach(element => {
-        const div_content = document.createElement("div");
-        div_content.id = "level";
-        div_content.className = "level";
-        document.getElementById("levels").appendChild(div_content);
-    
-        globEnum += 1;
-        let name = element.name;
-        let creator = element.creator;
-        let id = element.id;
-    
-        const pp = document.createElement("h1");
-        pp.textContent = "#"+globEnum+" - "+name+" ("+id+")"; // #1 - Test (616)
-        div_content.appendChild(pp);
-    
-        const p_blw = document.createElement("h3");
-        p_blw.textContent = "выложено "+creator; // published by karasikbtw
-        p_blw.style = "color: rgb(87, 87, 87, 0.75)";
-        div_content.appendChild(p_blw);
-    
-        console.log("test element created");
-    });
+    if (listname == document.title) {
+        levels.forEach(element => {
+            const div_content = document.createElement("div");
+            div_content.id = "level";
+            div_content.className = "level";
+            document.getElementById("levels").appendChild(div_content);
+        
+            globEnum += 1;
+            let name = element.name;
+            let creator = element.creator;
+            let id = element.id;
+        
+            const pp = document.createElement("h1");
+            pp.textContent = "#"+globEnum+" - "+name+" ("+id+")"; // #1 - Test (616)
+            div_content.appendChild(pp);
+        
+            const p_blw = document.createElement("h3");
+            p_blw.textContent = "выложено "+creator; // published by karasikbtw
+            p_blw.style = "color: rgb(87, 87, 87, 0.75)";
+            div_content.appendChild(p_blw);
+        
+            console.log("test element created");
+        });
+    };
 });
