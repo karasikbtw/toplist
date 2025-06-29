31cf68ac9e47e54545e976e1dc4f5ebcac027519
karasikbtw <drinjjy@gmail.com>
2025-06-29 15:06:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,35 @@ levels.forEach(element => {
 
     console.log("test element created");
 });
+
+let levels1 = [
+    { "name": "?", "creator": "noone", "id": "?" },
+    { "name": "?", "creator": "noone", "id": "?" },
+    { "name": "?", "creator": "noone", "id": "?" },
+    { "name": "?", "creator": "noone", "id": "?" },
+    { "name": "?", "creator": "noone", "id": "?" }
+];
+let globEnum1 = 0;
+
+levels1.forEach(element => {
+    const div_content = document.createElement("div");
+    div_content.id = "level";
+    div_content.className = "level";
+    document.getElementById("levels").appendChild(div_content);
+
+    globEnum1 += 1;
+    let name = element.name;
+    let creator = element.creator;
+    let id = element.id;
+
+    const pp = document.createElement("h1");
+    pp.textContent = "#"+globEnum1+" - "+name+" ("+id+")"; // #1 - Test (616)
+    div_content.appendChild(pp);
+
+    const p_blw = document.createElement("h3");
+    p_blw.textContent = "выложено "+creator; // published by karasikbtw
+    p_blw.style = "color: rgb(87, 87, 87, 0.75)";
+    div_content.appendChild(p_blw);
+
+    console.log("test element created");
+});
