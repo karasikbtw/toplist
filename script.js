c916f7d2e9c3ab385a81356de1c00e096379a122
karasikbtw <drinjjy@gmail.com>
2025-07-01 20:25:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -37,8 +37,8 @@ let lists = [
     ],
     [
         [ // levels
-            { "name": "8o", "below": "создатели: Bikchi? | верификатор: Prilka", "id": "287", "verifed": true },
-            { "name": "TT world", "below": "создатели: bikchi", "id": "136", "verifed": false },
+            { "name": "8o", "below": "создатели: Bikchi | верификатор: Prilka", "id": "287", "verifed": true },
+            { "name": "TT world", "below": "создатели: Bikchi", "id": "136", "verifed": false },
             { "name": "grimuar", "below": "создатели: redyzzz", "id": "223", "verifed": false },
             { "name": "ThisIsLikeThatSound", "below": "создатели: Taujaan", "id": "117", "verifed": false },
             { "name": "Electron", "below": "создатели: MamaYmerla", "id": "248", "verifed": false },
