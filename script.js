6967a1348fedc6dfdfcf826604492ad208b33751
karasikbtw <drinjjy@gmail.com>
2025-07-02 16:20:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ let lists = [
     ],
     [
         [ // players*
-            { "name": "DUHA5656", "below": "хардесты: TT world", "id": "", "verifed": false },
+            { "name": "DUHA5656", "below": "хардест в ГДПС: TT world | хардест в ГД: ?", "id": "", "verifed": false },
         ],
         { name: "Слеер лист - TopList", enum: 0, "href": "sleer-list.html", "displayname": " Слеер лист ", "raterequired": true, "ratevisibility": false }
     ]
