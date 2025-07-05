cf7ccf1304b1781817ea98b0af26dd3e7f3900f4
karasikbtw <drinjjy@gmail.com>
2025-07-05 12:59:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -64,8 +64,8 @@ let lists = [
     ],
     [
         [ // players*
-            { "name": "Prilka", "below": "хардест в ГДПС: 8o", "id": "", "verifed": true },
-            { "name": "DUHA5656", "below": "хардест демон в ГДПС: TT world | хардест челлендж в ГДПС: 1st DUHA5656 lvl | хардест в ГД: CraZy", "id": "", "verifed": true },
+            { "name": "Prilka", "below": "хардест в ГДПС: 8o | хардест в ГД: Jesse Pinkman", "id": "", "verifed": false },
+            { "name": "DUHA5656", "below": "хардест в ГДПС: TT world | хардест в ГД: CraZy", "id": "", "verifed": true },
         ],
         { name: "Слеер лист - TopList", enum: 0, "href": "sleer-list.html", "displayname": " Слеер лист ", "raterequired": true },
         { "rate": false, "moderators": true, "p_tag": true } // visibility
