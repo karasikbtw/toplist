e378ad60379696d930e83accdc680420a29bb222
karasikbtw <drinjjy@gmail.com>
2025-07-09 22:16:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ let lists = [
     [
         [ // players*
             { "name": "Prilka", "below": "хардест в ГДПС: 8o | хардест в ГД: Jesse Pinkman", "id": "", "verifed": true },
-            { "name": "DUHA5656", "below": "хардест в ГДПС: TT world | хардест в ГД: CraZy", "id": "", "verifed": true },
+            { "name": "DUHA5656", "below": "хардест в ГДПС: TT world | хардест в ГД: Acropolis", "id": "", "verifed": true },
             { "name": "nevverr", "below": "хардест в ГДПС: ordinary | хардест в ГД: Sonic Wave", "id": "", "verifed": false },
         ],
         { name: "Слеер лист - TopList", enum: 0, "href": "sleer-list.html", "displayname": " Слеер лист ", "raterequired": true },
