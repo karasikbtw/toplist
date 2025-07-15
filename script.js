0d385e2327fd82ac0e711b527bbdcb57f0e3186e
karasikbtw <drinjjy@gmail.com>
2025-07-15 21:55:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ let lists = [
             { "name": "Hayate", "below": "создатели: Taujaan", "id": "123", "verifed": false },
             { "name": "robotopa", "below": "создатели: komaru", "id": "343", "verifed": true },
             { "name": "16 SECONDS HELL", "below": "создатели: Honder", "id": "213", "verifed": false },
+            { "name": "Gibridge", "below": "создатели: WazalOwner", "id": "369", "verifed": false },
         ],
         { name: "Список челленджей - TopList", enum: 0, "href": "challenge-list.html", "displayname": " Список челленджей ", "raterequired": false },
         { "rate": true, "moderators": true, "p_tag": true } // visibility
