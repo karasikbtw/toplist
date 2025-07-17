de147fc032948873708ea9d7da78d9daeb93f9a3
karasikbtw <drinjjy@gmail.com>
2025-07-17 11:02:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ let lists = [
         [ // levels
             { "name": "KOCMOC", "below": "создатели: kisloteam | выложил: Kisl0", "id": "", "verifed": false },
             { "name": "firewall", "below": "создатели: anxiety team", "id": "", "verifed": false },
-            { "name": "telescope", "below": "создатели: ?", "id": "", "verifed": false },
+            { "name": "telescope", "below": "создатели: kisloteam", "id": "", "verifed": false },
         ],
         { name: "Невышедший лист - TopList", enum: 0, "href": "unloaded-list.html", "displayname": " Невышедший лист ", "raterequired": true },
         { "rate": true, "moderators": true, "p_tag": true } // visibility
